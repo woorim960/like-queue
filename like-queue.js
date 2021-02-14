e41bdf0337ea6331d41b20d28708c0b966b2a751
박우림 <woorimprog@gmail.com>
2021-02-14 22:27:34 +0900

    Implement: 큐의 초기 구조 구현

---
+++ like-queue.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class LikeQueue {
+  #idx;
+  #queue;
+
+  constructor() {
+    this.length = 0;
+    this.#idx = 0;
+    this.#queue = [];
+  }
+
+  enqueue(el) {
+    this.#queue.push(el);
+    this.length++;
+  }
+
+  dequeue() {
+    const firstElement = this.#queue[this.#idx];
+    this.length--;
+    this.#idx++;
+    return firstElement;
+  }
+}
